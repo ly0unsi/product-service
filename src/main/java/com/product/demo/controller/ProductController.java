162775c54141c094de55f57cc315c357eca5fc78
Abdllah <mcabdo25@gmail.com>
2024-03-03 15:57:17 +0100

    commit

--- src/main/java/com/product/demo/controller/ProductController.java
+++ src/main/java/com/product/demo/controller/ProductController.java
@@ -37,6 +37,6 @@ public class ProductController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello, world v7 hhhh";
+        return "Hello, world v8 hbibi";
     }
 }
